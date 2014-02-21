98f9c8d9ce32150188d2a71e3d10697684225271
ling0322 <ling032x@gmail.com>
2014-02-21 13:55:47 +0800

    add get_vocabulary.cc/.h

--- src/common/get_vocabulary.h
+++ src/common/get_vocabulary.h
@@ -21,45 +21,41 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 //
-// crf_vocab.cc --- Created at 2014-01-28
+// get_vocabulary.h --- Created at 2014-02-21
 //
 
-#include "neko/crf_vocab.h"
+#ifndef SRC_COMMON_GET_VOCABULARY_H_
+#define SRC_COMMON_GET_VOCABULARY_H_
+
+#include <stdint.h>
 #include <unordered_map>
-#include <string>
-#include <thread>
-#include "common/get_vocabulary.h"
 #include "milkcat/milkcat.h"
-#include "utils/readable_file.h"
 #include "utils/status.h"
 
 namespace milkcat {
 
-
 // Segment the corpus from path and return the vocabulary of chinese words.
 // If any errors occured, status is not Status::OK()
-std::unordered_map<std::string, int> GetCrfVocabulary(
+//
+// Parameters:
+//   path           - The path of file to be analyze
+//   model          - The model that would be used by analyzer
+//   analyzer_type  - The type of analyzer  
+//   n_threads      - Number of threads
+//   total_count    - Word count in file
+//   progress       - A callback function to report progress
+//   status         - Indicates if any error occured
+std::unordered_map<std::string, int> GetVocabularyFromFile(
     const char *path,
+    milkcat_model_t *model,
+    int analyzer_type,
+    int n_threads,
     int *total_count,
     void (* progress)(int64_t bytes_processed,
                       int64_t file_size,
                       int64_t bytes_per_second),
-    Status *status) {
-
-  std::unordered_map<std::string, int> vocab;
-  milkcat_model_t *model = milkcat_model_new(NULL);
-
-  vocab = GetVocabularyFromFile(path,
-                                model,
-                                CRF_SEGMENTER,
-                                std::thread::hardware_concurrency(),
-                                total_count,
-                                progress,
-                                status);
-
-  milkcat_model_destroy(model);
-
-  return vocab;
-}
+    Status *status);
 
 }  // namespace milkcat
+
+#endif  // SRC_COMMON_GET_VOCABULARY_H_
